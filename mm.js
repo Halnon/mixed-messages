2f0bf55ecc86ff6179eb190367e1332e4914dd42
Adam Halnon <halnon_01@hotmail.com>
2024-03-12 13:16:45 +0000

    amend apostraphies in jokes array

--- mm.js
+++ mm.js
@@ -1,15 +1,16 @@
+//object with properties for randommmessage generator
 const msgObj = {
      jokes: [
         'What do you call a pony with a cough? A little horse.',
         'What do you call a magic dog? A labracadabrador.',
         'What did the shark say when he ate the clownfish? This tastes a little funny.',
-        "What’s orange and sounds like a carrot? A parrot.",
-        "Why can’t you hear a pterodactyl go to the bathroom? Because the “P” is silent.",
+        "What's orange and sounds like a carrot? A parrot.",
+        "Why can't you hear a pterodactyl go to the bathroom? Because the “P” is silent.",
         'What do you call a woman with one leg? Eileen.',
         'What did the pirate say when he turned 80? Aye matey.',
         'Why did the frog take the bus to work today? His car got toad away.',
         'What did the buffalo say when his son left for college? Bison.',
-        "What is an astronaut’s favorite part on a computer? The space bar.",
+        "What is an astronaut's favorite part on a computer? The space bar.",
         'Why did the yogurt go to the art exhibition? Because it was cultured.',
         'What do you call an apology written in dots and dashes? Re-Morse code.'
     ],
